f257f57f5d41e471bc7e8bfd463a080594b17561
William Kulha <kulha.william@gmail.com>
2018-09-10 12:41:40 -0400

    Add all the different forms the JS required to return the user inputted values.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  var cooking = $("#cooking").val();
+
+  var hobby = $("input:radio[name=hobby]:checked").val();
+
+  var dob = $("#born").val();
+
+  var favoritecolor = $("#color").val();
+
+  debugger;
+
+});
